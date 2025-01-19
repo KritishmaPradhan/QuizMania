7c625f60a93ad25e87bd769896abe55be696af26
KritishmaPradhan <159471939+KritishmaPradhan@users.noreply.github.com>
2025-01-19 18:13:56 +0545

    thirdcommit

--- quizapp/views.py
+++ quizapp/views.py
@@ -4,11 +4,16 @@ from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-         username = request.POST.get('name')
-         my_user = User.objects.create_user(username)
-         print(username)
-         my_user.save()
-         return render(request,'nextpage.html', {'user': username})
+        username = request.POST.get('name')
+        if not username:
+            return render(request, 'index.html', {'error': 'Username cannot be empty'})
+        # Check if the username already exists
+        if User.objects.filter(username=username).exists():
+            return render(request, 'index.html', {'error': 'Username already exists'})
+        else:
+            my_user = User.objects.create_user(username)
+            my_user.save()
+            return render(request,'nextpage.html', {'user': username})
     return render(request, 'index.html')
 def nextpage(request):
     return render(request, 'nextpage.html')
